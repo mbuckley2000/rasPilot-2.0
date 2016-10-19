18df13387f98e95142dcec6d38c1b9c51a1c26e3
matt <mbuckley2000@gmail.com>
2016-10-19 01:11:24 +0100

    .

--- src/Main.cpp
+++ src/Main.cpp
@@ -39,7 +39,7 @@ int main() {
     IMUPositionEstimater positionEstimater = IMUPositionEstimater(&imu);
 
     PiCam cam;
-    cam.arucoCap();
+    //cam.arucoCap();
 
     while (true) {
         stabiliser.update();
